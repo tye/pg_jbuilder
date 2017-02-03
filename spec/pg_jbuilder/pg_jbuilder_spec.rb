b809d8525f72630a101189cc183a92e69bb8916d
tye <tye@tiz.com>
2017-02-02 22:22:13 -0500

    Use erb templates

--- spec/pg_jbuilder/pg_jbuilder_spec.rb
+++ spec/pg_jbuilder/pg_jbuilder_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PgJbuilder, without_rails: true do
+  after(:each) { PgJbuilder.clear_cache }
   describe '.connection' do
     subject { PgJbuilder.connection }
     it 'saves the connection' do
@@ -74,7 +75,7 @@ describe PgJbuilder, without_rails: true do
       end
       subject { PgJbuilder.render('template',defined?(variables) ? variables : {}) }
       context 'with variables' do
-        let(:template) {"{{variable1}},{{variable2}}"}
+        let(:template) {"<%= variable1 %>,<%= variable2 %>"}
         let(:variables) {
           {variable1: 'value_1',
            variable2: 'value_2'}
@@ -95,7 +96,7 @@ describe PgJbuilder, without_rails: true do
       end # that has variables
 
       describe 'include helper' do
-        let(:template) {"Included: {{include 'included_template'}}"}
+        let(:template) {"Included: <%= include 'included_template' %>"}
         let(:included_template) {"Included Template Contents"}
         before :each do
           allow(PgJbuilder).to receive(:get_query_contents).
@@ -110,14 +111,14 @@ describe PgJbuilder, without_rails: true do
             {variable1: 'value_1',
              variable2: 'value_2'}
           }
-          let(:included_template) {"Included Template {{variable1}},{{variable2}}"}
+          let(:included_template) {"Included Template <%= variable1 %>,<%= variable2 %>"}
           it 'the included template inherits the variables' do
             is_expected.to eq("Included: Included Template value_1,value_2")
           end
 
           context 'with variables in the include' do
-            let(:template) {"Included: {{include 'included_template' variable3='value_3' variable4='value_4'}}"}
-            let(:included_template) {"Included Template {{variable1}},{{variable2}},{{variable3}},{{variable4}}"}
+            let(:template) {"Included: <%= include 'included_template', variable3: 'value_3', variable4: 'value_4' %>"}
+            let(:included_template) {"Included Template <%= variable1 %>,<%= variable2 %>,<%= variable3 %>,<%= variable4 %>"}
             it 'the included template has the variables' do
               is_expected.to eq("Included: Included Template value_1,value_2,value_3,value_4")
             end
@@ -126,7 +127,7 @@ describe PgJbuilder, without_rails: true do
       end
 
       describe 'quote helper' do
-        let(:template) { 'Quoted: {{quote variable1}}' }
+        let(:template) { 'Quoted: <%= quote variable1 %>' }
         let(:variables) do
           {variable1: 'String'}
         end
@@ -136,12 +137,12 @@ describe PgJbuilder, without_rails: true do
       end
 
       describe 'object helper' do
-        let(:template) { "Object: {{#object}}\nObject Test\n{{/object}}" }
+        let(:template) { "Object: <% object do %>\nObject Test\n<% end %>" }
         it 'renders object sql' do
           is_expected.to eq("Object: (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (\nObject Test\n)object_row)")
         end
         context 'with template name' do
-          let(:template) { "Object: {{object 'object_template'}}" }
+          let(:template) { "Object: <%= object 'object_template' %>" }
           let(:object_template) do
             "Object Template"
           end
@@ -157,13 +158,13 @@ describe PgJbuilder, without_rails: true do
       end # object helper
 
       describe 'array helper' do
-        let(:template) { "Array: {{#array}}\nArray Test\n{{/array}}" }
+        let(:template) { "Array: <% array do %>\nArray Test\n<% end %>" }
         it 'renders array sql' do
           is_expected.to eq("Array: (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (\nArray Test\n)array_row)")
         end
 
         context 'with template name' do
-          let(:template) { "Array: {{array 'array_template'}}" }
+          let(:template) { "Array: <%= array 'array_template' %>" }
           let(:array_template) do
             "Array Template"
           end
